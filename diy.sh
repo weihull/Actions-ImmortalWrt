072b9cc5b539c05c5e7414036e86098c1081c175
weihull <147634476+weihull@users.noreply.github.com>
2025-10-21 21:06:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,16 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
+
+# 编辑默认的lan口ip地址
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
+# 编辑默认的主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 编辑默认的luci显示的固件名称
+#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
+
+# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
